78472f36ba55e57c1159c2df51722f1264d5b722
DankeMary <laktionofs@mail.ru>
2018-10-24 23:25:01 +0300

    AppConfig class updated for error handling

--- sbb-war/src/main/java/net/tsystems/configuration/ApplicationInitializer.java
+++ sbb-war/src/main/java/net/tsystems/configuration/ApplicationInitializer.java
@@ -1,15 +1,15 @@
 package net.tsystems.configuration;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
 
@@ -23,7 +23,9 @@ public class ApplicationInitializer implements WebApplicationInitializer {
         servletContext.addListener(new ContextLoaderListener(annotationConfigWebApplicationContext));
         servletContext.addListener(new RequestContextListener());
 
-        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(annotationConfigWebApplicationContext));
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(annotationConfigWebApplicationContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", dispatcherServlet);
         dynamic.addMapping("/");
         dynamic.setLoadOnStartup(NumberUtils.INTEGER_ONE);
     }

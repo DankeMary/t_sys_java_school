d615d08a7a926f72d101c51b0f9ba5630e533fb0
DankeMary <laktionofs@mail.ru>
2018-11-04 19:52:39 +0300

    Added mapping list methods

--- dataobjects/src/main/java/net/tsystems/entitymapper/TripDataEntityMapper.java
+++ dataobjects/src/main/java/net/tsystems/entitymapper/TripDataEntityMapper.java
@@ -4,8 +4,12 @@ import net.tsystems.entities.TripDataDO;
 import net.tsystems.serviceobject.TripDataSO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface TripDataEntityMapper {
     TripDataSO tripDataToSO(TripDataDO tdDO);
     TripDataDO tripDataToDO(TripDataSO tdSO);
+    List<TripDataSO> tripDataListToSOList(List<TripDataDO> tdListDO);
+    List<TripDataDO> tripDataListToDOList(List<TripDataSO> tdListSO);
 }

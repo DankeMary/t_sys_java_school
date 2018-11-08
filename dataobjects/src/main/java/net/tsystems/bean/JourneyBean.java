692a724401056078a4a4a4b9c45d447fe2f048be
DankeMary <laktionofs@mail.ru>
2018-11-08 15:32:48 +0300

    journeyId field added to JourneyBean

--- dataobjects/src/main/java/net/tsystems/bean/JourneyBean.java
+++ dataobjects/src/main/java/net/tsystems/bean/JourneyBean.java
@@ -5,11 +5,21 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.util.Date;
 
 public class JourneyBean {
+    private int journeyId;
     private TripBean trip;
     //private int routeId;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date departureDay;
     //TODO seats left, canceleld, late
+
+
+    public int getJourneyId() {
+        return journeyId;
+    }
+
+    public void setJourneyId(int journeyId) {
+        this.journeyId = journeyId;
+    }
 
     public TripBean getTrip() {
         return trip;
@@ -18,8 +28,6 @@ public class JourneyBean {
     public void setTrip(TripBean trip) {
         this.trip = trip;
     }
-
-
 
     /*public int getRouteId() {
         return routeId;

0e882331080eac43179eb4a6b8a62248018324de
DankeMary <laktionofs@mail.ru>
2018-11-10 21:40:16 +0300

    LocalDateMapper usage added

--- dataobjects/src/main/java/net/tsystems/entitymapper/TripDataEntityMapper.java
+++ dataobjects/src/main/java/net/tsystems/entitymapper/TripDataEntityMapper.java
@@ -2,11 +2,13 @@ package net.tsystems.entitymapper;
 
 import net.tsystems.entities.TripDataDO;
 import net.tsystems.serviceobject.TripDataSO;
+import net.tsystems.utilmapper.LocalDateMapper;
 import org.mapstruct.Mapper;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@Mapper
+@Mapper(uses= LocalDateMapper.class)
 public interface TripDataEntityMapper {
     TripDataSO tripDataToSO(TripDataDO tdDO);
     TripDataDO tripDataToDO(TripDataSO tdSO);

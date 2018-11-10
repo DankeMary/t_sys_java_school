7e1a3f18d74203c7e8b70c5fa2c38937f2837170
DankeMary <laktionofs@mail.ru>
2018-11-10 22:07:07 +0300

    Timing issue fixed (output today but after current time)

--- database/src/main/java/net/tsystems/impl/TripDataDAOImpl.java
+++ database/src/main/java/net/tsystems/impl/TripDataDAOImpl.java
@@ -10,16 +10,16 @@ import java.util.List;
 @Repository
 public class TripDataDAOImpl extends AbstractDaoImpl<TripDataDO, Integer> implements TripDataDAO {
     public List<TripDataDO> findFirstByTrain(int id) {
-        List<TripDataDO> list = (List<TripDataDO>)getEntityManager()
+        List<TripDataDO> list = (List<TripDataDO>) getEntityManager()
                 .createQuery("from TripDataDO where route.trip.train=" + id
-                                        + " and route.station = route.trip.from")
+                        + " and route.station = route.trip.from")
                 .list();
         return list.size() == 0 ? null : list;
     }
 
     @Override
     public List<TripDataDO> findFirstAfterNowByTrain(int id) {
-        List<TripDataDO> list = (List<TripDataDO>)getEntityManager()
+        List<TripDataDO> list = (List<TripDataDO>) getEntityManager()
                 .createQuery("from TripDataDO where route.trip.train=" + id
                         + " and route.station = route.trip.from"
                         + " and date >= now()")
@@ -29,7 +29,7 @@ public class TripDataDAOImpl extends AbstractDaoImpl<TripDataDO, Integer> implem
 
     @Override
     public List<TripDataDO> findByTrainIdAndTripDepartureDay(int trainId, Date date) {
-        List<TripDataDO> list = (List<TripDataDO>)getEntityManager()
+        List<TripDataDO> list = (List<TripDataDO>) getEntityManager()
                 .createQuery("from TripDataDO td where td.route.trip.train.id=" + trainId
                         + " and date=" + date)
                 .list();
@@ -38,13 +38,18 @@ public class TripDataDAOImpl extends AbstractDaoImpl<TripDataDO, Integer> implem
 
     @Override
     public List<TripDataDO> getScheduleForStation(String stationName, int maxResults) {
-        List<TripDataDO> list = (List<TripDataDO>)getEntityManager()
+        List<TripDataDO> list = (List<TripDataDO>) getEntityManager()
                 .createQuery("select td from TripDataDO td " +
                         "join RouteDO r on td.route = r " +
                         "join TripDO t on r.trip = t " +
                         "join TrainDO tr on t.train = tr " +
-                        "where r.station.name =\'" + stationName +"\' and " +
-                        " td.date >= now() " +
+                        "where r.station.name =\'" + stationName + "\' and " +
+                        " (td.date > current_date() or" +
+                        " (td.date = current_date() and " +
+                        "((hour(r.arrival) >= hour(now()) and" +
+                        " minute(r.arrival) >= minute(now())) or " +
+                        "((hour(r.departure) >= hour(now()) and " +
+                        " minute(r.departure) >= minute(now()))))))" +
                         " order by td.date, r.arrival")
                 .setMaxResults(maxResults)
                 .list();

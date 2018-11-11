fe55700dd555d6b76b59f96f769a4dbf11b178f0
DankeMary <laktionofs@mail.ru>
2018-11-11 19:54:44 +0300

    Query in getDataForSection fixed

--- database/src/main/java/net/tsystems/impl/TripDataDAOImpl.java
+++ database/src/main/java/net/tsystems/impl/TripDataDAOImpl.java
@@ -62,52 +62,50 @@ public class TripDataDAOImpl extends AbstractDaoImpl<TripDataDO, Integer> implem
     //TODO time? (type)
     @Override
     //@SuppressWarnings("unchecked")
-    public List<TripDataDO> getDataForSection (Date fromDay,
-                                               Time fromTime,
-                                               Date toDay,
-                                               Time toTime,
-                                               String fromStation,
-                                               String toStation) {
+    public List<TripDataDO> getDataForSection(Date fromDay,
+                                              Time fromTime,
+                                              Date toDay,
+                                              Time toTime,
+                                              String fromStation,
+                                              String toStation) {
         //TODO check that dates and times are after right now?
 
         List<TripDataDO> list = (List<TripDataDO>) getEntityManager()
-                .createQuery("select td1 from TripDataDO td1 " +
-                                "where exists (" +
-                                "select td from TripDataDO td " +
-                                " join RouteDO r on td.route = r  " +
-                                "join TripDO t on r.trip = t  " +
-                                "join TrainDO tr on t.train = tr  " +
-                                "where td.id = td1.id and " +
-                                "r.station.name = \'" + fromStation + "\' and  " +
-                                "td.date between \'" + fromDay + "\' and \'" + toDay + "\' and " +
-                                //td.date == today == fromDay == toDay
-                                "((td.date = \'" + fromDay + "\' and \'" + fromDay + "\' = \'" + toDay + "\' and " +
-                                "(time(r.departure) between \'" + fromTime + "\' and \'" + toTime + "\'" +
-                                " or time(r.departure) between time(now()) and \'" + toTime + "\\') or " +
-                                //TODO Remove repetitive part?
-                                //td.date == today == fromDay
-                                "((\'" + fromDay + "\' = current_date() and td.date = \'" + fromDay + "\' and " +
-                                "time(r.departure) >= time(now()) and " +
-                                "time(r.departure) >= \'" + fromTime + "\') " +
-                                " or " +
-                                //td.date == fromDay
-                                "(td.date = \'" + fromDay + "\' and " +
-                                "time(r.departure) >= \'" + fromTime + "\')" +
-                                " or " +
-                                //td.date in (fromDay, toDay)
-                                "(td.date > " + fromDay + " and " +
-                                "td.date < " + toDay + ") " +
-                                " or " +
-                                //td.date == toDay
-                                "(td.date = " + toDay + " and " +
-                                "time(r.departure) < \'" + toTime + "\'))))" +
-                                " and " +
-                                " exists (select td2 from TripDataDO td2  " +
-                                "join RouteDO r2 on td2.route = r2  " +
-                                "join TripDO t2 on r2.trip = t2  " +
-                                "join TrainDO tr2 on t2.train = tr2  " +
-                                "where td1.tripDeparture = td2.tripDeparture and r2.station.name = \'" + toStation + "\' )"
-                        )
+                .createQuery(
+                        "select td1 from TripDataDO td1 " +
+                        "where exists (" +
+                        "select td from TripDataDO td " +
+                        " join RouteDO r on td.route = r  " +
+                        "join TripDO t on r.trip = t  " +
+                        "join TrainDO tr on t.train = tr  " +
+                        "where td.id = td1.id and " +
+                        "r.station.name = \'" + fromStation + "\' and  " +
+                        "td.date between \'" + fromDay + "\' and \'" + toDay + "\' and " +
+                        //td.date == today == fromDay == toDay
+                        "((td.date = \'" + fromDay + "\' and \'" + fromDay + "\' = \'" + toDay + "\' and " +
+                        "\'" + fromDay + "\' = current_date() and " +
+                        "(time(r.departure) between \'" + fromTime + "\' and \'" + toTime + "\'" +
+                        " or time(r.departure) between time(now()) and \'" + toTime + "\')) " +
+                        "or " +
+                        //TODO Remove repetitive part?
+                        //td.date == today == fromDay
+                        "(\'" + fromDay + "\' = current_date() and td.date = \'" + fromDay + "\' and " +
+                        "time(r.departure) >= time(now()) and time(r.departure) >= \'" + fromTime + "\') " +
+                        " or " +
+                        //td.date == fromDay
+                        "(td.date = \'" + fromDay + "\' and time(r.departure) >= \'" + fromTime + "\')" +
+                        " or " +
+                        //td.date in (fromDay, toDay)
+                        "(td.date > " + fromDay + " and td.date < " + toDay + ") " +
+                        " or " +
+                        //td.date == toDay
+                        "(td.date = " + toDay + " and time(r.departure) < \'" + toTime + "\')))" +
+                        " and exists (select td2 from TripDataDO td2  " +
+                        "join RouteDO r2 on td2.route = r2  " +
+                        "join TripDO t2 on r2.trip = t2  " +
+                        "join TrainDO tr2 on t2.train = tr2  " +
+                        "where td1.tripDeparture = td2.tripDeparture and r2.station.name = \'" + toStation + "\' )"
+                )
                 .list();
         return list;
     }

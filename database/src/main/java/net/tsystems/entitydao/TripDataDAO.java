a4a62e0340b1a79a0cbf2a598e38798cb13e6e37
DankeMary <laktionofs@mail.ru>
2018-11-11 16:53:02 +0300

    Query for searching trains that go from A to B between <date1 : time1> and <date2 : time2> added

--- database/src/main/java/net/tsystems/entitydao/TripDataDAO.java
+++ database/src/main/java/net/tsystems/entitydao/TripDataDAO.java
@@ -4,6 +4,7 @@ import net.tsystems.AbstractDao;
 import net.tsystems.entities.TripDataDO;
 
 import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
 public interface TripDataDAO extends AbstractDao<TripDataDO, Integer> {
@@ -11,4 +12,10 @@ public interface TripDataDAO extends AbstractDao<TripDataDO, Integer> {
     List<TripDataDO> findFirstAfterNowByTrain(int id);
     List<TripDataDO> findByTrainIdAndTripDepartureDay (int trainId, Date date);
     List<TripDataDO> getScheduleForStation(String stationName, int maxResults);
+    List<TripDataDO> getDataForSection (Date fromDay,
+                                        Time fromTime,
+                                        Date toDay,
+                                        Time toTime,
+                                        String fromStation,
+                                        String toStation);
 }

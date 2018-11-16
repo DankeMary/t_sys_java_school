3726e088e3e07953ee113f7281790ee86c5f77aa
DankeMary <laktionofs@mail.ru>
2018-11-16 12:47:25 +0300

    Added method for returning the new object after creating one

--- database/src/main/java/net/tsystems/entitydao/PassengerDAO.java
+++ database/src/main/java/net/tsystems/entitydao/PassengerDAO.java
@@ -6,5 +6,5 @@ import net.tsystems.entities.PassengerDO;
 
 
 public interface PassengerDAO extends AbstractDao<PassengerDO, Integer> {
-
+    PassengerDO createReturnObject(PassengerDO p);
 }
--- database/src/main/java/net/tsystems/impl/PassengerDAOImpl.java
+++ database/src/main/java/net/tsystems/impl/PassengerDAOImpl.java
@@ -6,5 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class PassengerDAOImpl extends AbstractDaoImpl<PassengerDO, Integer> implements PassengerDAO {
-
+    @Override
+    public PassengerDO createReturnObject(PassengerDO p) {
+        getEntityManager().persist(p);
+        return p;
+    }
 }

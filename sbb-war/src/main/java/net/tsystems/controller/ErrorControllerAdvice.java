a6c3e1bc5a5ee9cbb311dbe5e748b67eb702dcf5
DankeMary <laktionofs@mail.ru>
2018-11-16 20:16:09 +0300

    Generic Error handler added

---
+++ sbb-war/src/main/java/net/tsystems/controller/ErrorControllerAdvice.java
@@ -0,0 +1,21 @@
+package net.tsystems.controller;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ErrorControllerAdvice {
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ExceptionHandler(value = {Exception.class, RuntimeException.class})
+    public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) {
+        ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
+
+        mav.addObject("message", "Oups! Something went wrong");
+        return mav;
+    }
+}

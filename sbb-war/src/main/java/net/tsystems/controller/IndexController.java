3d2b47c02f2c7a3788885cb737547518af817db3
DankeMary <laktionofs@mail.ru>
2018-11-23 23:52:35 +0300

    Minor changes (mapping and clean)

--- sbb-war/src/main/java/net/tsystems/controller/IndexController.java
+++ sbb-war/src/main/java/net/tsystems/controller/IndexController.java
@@ -8,13 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String newTrain(Model model) {
         return "redirect:/passengers";
-    }
-
-    @RequestMapping(value = "/future", method = RequestMethod.GET)
-    public String future(Model model) {
-        return "future";
     }
 }
